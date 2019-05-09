37ae37451fece994b17fcb4bbee7b9c7557ee023
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-09 09:39:50 -0500

    POST to JSONplaceholder api added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
+import Postform from "./components/Postform";
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <Postform />
+      <hr />
       <Posts />
     </div>
   );
